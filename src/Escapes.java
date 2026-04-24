718a07ea29c9da745e32289a5e83e6783674b052
akshay3432 <akshayakki3432@gmail.com>
2026-04-24 13:28:44 +0530

    2nd change

---
+++ src/Escapes.java
@@ -0,0 +1,12 @@
+public class Escapes {
+
+    public static void main(String[]args ){
+        System.out.println(" Hello \"Akshay\" ") ; //escape sequence for putting double commas
+        System.out.println(" Hello \nAkshay ") ;//for next line
+        System.out.println(" Hello \tAkshay ") ;//for tab or space
+        System.out.println(" Hello\bAkshay ") ;
+
+
+
+    }
+}
