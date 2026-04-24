10fb2195ed3c307b6785506cd5bf112772ce9dbd
akshay3432 <akshayakki3432@gmail.com>
2026-04-24 12:43:44 +0530

    first change

---
+++ src/Variables.java
@@ -0,0 +1,27 @@
+public class Variables {
+
+     public static void main(String[]args){
+
+         int myNumber = 78;  //Declaration of Var and Initialisation
+
+         int yourNumber; // Declaration of Variables
+          yourNumber = 865; // initialisation
+         System.out.println(yourNumber);
+         yourNumber = 56;
+         System.out.println(yourNumber);
+         System.out.println(myNumber);
+
+
+         boolean isVegatarian = true;
+         System.out.println(isVegatarian);
+         float myFloat = 5.0f;
+         System.out.println(myFloat);
+         double myDouble = 6.8787;
+         System.out.println(myDouble);
+
+         String wishes = "Good MORNING";
+         System.out.println(wishes); // String as a Variables
+
+
+     }
+}
