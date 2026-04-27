14b9d33f8492dfc298232aa4dc21b780ddd2aa9d
akshay3432 <akshayakki3432@gmail.com>
2026-04-27 14:34:46 +0530

    Casting

---
+++ src/TypeConversion.java
@@ -0,0 +1,14 @@
+public class TypeConversion {
+    public static void main(String[]args){
+        float myFloat =  5 ;
+        // implicit conversion or coercion.It is automatic as data type
+         //is bigger and literal is smaller type
+        System.out.println(myFloat);
+         int myInt = (int)5.0f;
+         System.out.println(myInt);
+         //casting or explicit conversion
+
+    }
+
+
+}
